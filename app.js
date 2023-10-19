27df9cc01314ee455cc4aa3e1a06754f285a6816
samade747 <samad.e747@gmail.com>
2023-10-20 02:39:02 +0500

    22

--- app.js
+++ app.js
@@ -1,9 +1,33 @@
 //
-// get attribute get the data by id 
-function foo(){
-    var a = document.getElementById('inp');
-    var b = a.getAttribute('name');
-    console.log(b)
+// // get attribute get the data by id 
+// function foo(){
+//     var a = document.getElementById('inp');
+//     var b = a.getAttribute('name');
+//     console.log(b)
 
-}
+// }
+
+
+//
+// has attribute check the attribute is available or not 
+// function foo(){
+//     var a = document.getElementById('inp');
+//     var b = a.hasAttribute('class');
+//     console.log(b)
+
+// }
+
+
+
+
+//
+// set attribute ke means hain agaar nai hai toh lga toh Attribute 
+// function foo(){
+//     var a = document.getElementById('inp');
+//     var b = a.setAttribute('class', 'aaa');
+//     console.log(b)
+
+// }
+
+
 
