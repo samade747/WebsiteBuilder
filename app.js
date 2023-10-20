14adb4232d7919a67720976b0977197d473e592f
samade747 <samad.e747@gmail.com>
2023-10-21 02:35:00 +0500

    11512

--- app.js
+++ app.js
@@ -30,4 +30,31 @@
 // }
 
 
+// Navigation Bar full Resposive
 
+let homepages = document.querySelector('.home-pages');
+let SocialMedia = document.querySelector('.Social-Media');
+let togglebtn = document.querySelector('.toggle-btn');
+let mainnvabar = document.querySelector('.main-nav-bar');
+let logoone = document.querySelector('.logo-one');
+
+function AutoResizing(){
+    const mainwindow = window.innerWidth;
+    if(mainwindow <= 997){
+        homepages.style.display = 'none';
+        SocialMedia.style.display = 'none';
+        togglebtn.style.display = 'block';
+        mainnvabar.style.padding = '30px';
+        logoone.style.color = 'blue';
+    } else {
+        homepages.style.display = 'block';
+        SocialMedia.style.display = 'flex';
+        togglebtn.style.display = 'none'
+        mainnvabar.style.padding = '0';
+        logoone.style.color = 'red';
+    }
+}
+
+AutoResizing()
+
+window.addEventListener('resize', AutoResizing);
